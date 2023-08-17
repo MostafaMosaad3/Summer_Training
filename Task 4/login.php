15644a07af4415177658a2a83919d55f3ce62054
Mostafa Mosaad <121785781+MostafaMosaad3@users.noreply.github.com>
2023-08-17 07:13:28 +0300

    Add files via upload

---
+++ Task 4/login.php
@@ -0,0 +1,16 @@
+
+<html>
+<head>
+    <title>Login page</title>
+</head>
+<body>
+<h1>login here</h1>
+<form action="session.php" method="post">
+    <input type="text" name="username" placeholder="Enter your name" required>
+    <input type="password" name="password" placeholder="Enter your password" required >
+    <input type="checkbox" name="remember me" value="1" >
+    <input type="submit" value="Submit" name="submit">
+</form>
+</body>
+</html>
+
---
+++ Task 4/session.php
@@ -0,0 +1,41 @@
+<?php
+
+//extension
+$users = json_decode(file_get_contents('users.txt'), true);
+
+
+session_start();
+
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $remember = $_POST['remember'];
+
+
+    //loop on file
+    foreach ($users as $user) {
+        // check if key exist then password is same
+        ;
+        if (array_key_exists($username, $user) && $user[$username] == $password)
+        {
+            $_SESSION['username'] = $username;
+            header('location:todo list/index.php');
+        }
+        else
+        {
+            echo "username or password not correct";
+            header('refresh:5;location:login.php');
+            exit();
+        }
+    }
+}
+else {
+ echo "username or password not set";
+ header('refresh:5;location:login.php');
+ exit();
+
+
+
+}
